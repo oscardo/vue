3d4332616009b52508638e0a712a8452eeb195a3
Oscardo <oscardo2000@gmail.com>
2023-11-21 21:13:16 -0500

    cambios básicos

--- global-vuex/src/store/index.ts
+++ global-vuex/src/store/index.ts
@@ -2,13 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    counter: 1
   },
   getters: {
+    times2(state){
+      return state.counter * 2
+    }
   },
   mutations: {
+    setCounter(state, value){
+      state.counter = value
+    }
   },
   actions: {
+    // async setCounter(state, value){
+    //   await state.counter = value
+    // }
   },
   modules: {
+    //no lo estamos usando
   }
 })
